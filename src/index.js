bd5c5db000c6fa61d6c1ed0c23a12bcceb42018d
Trard <trardone@gmail.com>
2022-06-01 14:17:37 +0300

    try higher

--- src/index.js
+++ src/index.js
@@ -12,27 +12,27 @@ let {
     group_id,
     cookies,
     authorization,
-    timeout
+    timeout,
 } = config;
 
 const vk = new VK({
     token: access_token,
 });
 
-var string_cookies = stringifyCookies(cookies)
+var string_cookies = stringifyCookies(cookies);
 
 var hash;
 
 (async () => {
-    hash = await getHash(cookies, authorization, group_id, group_chat_id)
-})()
+    hash = await getHash(cookies, authorization, group_id, group_chat_id);
+})();
 
 function stringifyCookies(cookies) {
-    let string_cookies = ""
+    let string_cookies = "";
 
     cookies.map((cookie) => {
-        string_cookies += `${cookie.name}=${cookie.value}; `
-    })
+        string_cookies += `${cookie.name}=${cookie.value}; `;
+    });
 
     return string_cookies;
 }
@@ -45,20 +45,28 @@ async function getHash(cookies, authorization, group_id, group_chat_id) {
     await page.setExtraHTTPHeaders({
         "user-agent":
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0",
-        "Authorization": authorization,
+        Authorization: authorization,
     });
     await page.goto(`https://vk.com/gim${group_id}?sel=c${group_chat_id}`);
 
     let content = await page.content();
+    await page.screenshot({ path: "exampl.png" });
     let regex = /IM\.init(.(?<!"hash":"))+"((.(?<!"))*)"/;
     let hash = regex.exec(content)[2];
 
     browser.close();
 
     return hash;
-};
+}
 
-async function add_to_chat(user_id, chat_id, your_id, group_chat_id, group_id, hash) {
+async function add_to_chat(
+    user_id,
+    chat_id,
+    your_id,
+    group_chat_id,
+    group_id,
+    hash
+) {
     await vk.api.groups
         .unban({
             group_id: group_id,
@@ -69,24 +77,27 @@ async function add_to_chat(user_id, chat_id, your_id, group_chat_id, group_id, h
                 throw e;
             }
         });
-    
+
     await fetch("https://vk.com/al_im.php?act=a_toggle_admin", {
         headers: {
-            "accept": "*/*",
+            accept: "*/*",
             "accept-language": "en,ru;q=0.9",
             "content-type": "application/x-www-form-urlencoded",
-            "sec-ch-ua": '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
+            "sec-ch-ua":
+                '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
             "sec-ch-ua-mobile": "?0",
             "sec-ch-ua-platform": '"Windows"',
             "sec-fetch-dest": "empty",
             "sec-fetch-mode": "cors",
             "sec-fetch-site": "same-origin",
             "x-requested-with": "XMLHttpRequest",
-            "cookie": string_cookies,
-            "Referer": `https://vk.com/gim${group_id}?sel=c${group_chat_id}`,
+            cookie: string_cookies,
+            Referer: `https://vk.com/gim${group_id}?sel=c${group_chat_id}`,
             "Referrer-Policy": "strict-origin-when-cross-origin",
         },
-        body: `act=a_toggle_admin&al=1&chat=${2000000000 + group_chat_id}&gid=${group_id}&hash=${hash}&im_v=3&is_admin=1&mid=${your_id}`,
+        body: `act=a_toggle_admin&al=1&chat=${
+            2000000000 + group_chat_id
+        }&gid=${group_id}&hash=${hash}&im_v=3&is_admin=1&mid=${your_id}`,
         method: "POST",
     });
 
@@ -119,23 +130,35 @@ async function isUserInChat(chat_id, user_id) {
 }
 
 function main() {
-        let interval = setInterval(async () => {
+    let interval = setInterval(async () => {
+        try {
             if (!(await isUserInChat(chat_id, user_id))) {
-                try {
-                    await add_to_chat(user_id, chat_id, your_id, group_chat_id, group_id, hash);
-                    console.log("added");
-                } catch (e) {
-                    console.log("catched")
-                    clearInterval(interval);
-            
-                    (async () => {
-                        hash = await getHash(cookies, authorization, group_id, group_chat_id)
-                    })();
-            
-                    setTimeout(main, 5 * 60 * 1000);
-                }
+                await add_to_chat(
+                    user_id,
+                    chat_id,
+                    your_id,
+                    group_chat_id,
+                    group_id,
+                    hash
+                );
+                console.log("added");
             }
-        }, timeout)
+        } catch (e) {
+            console.log("catched");
+            clearInterval(interval);
+
+            (async () => {
+                hash = await getHash(
+                    cookies,
+                    authorization,
+                    group_id,
+                    group_chat_id
+                );
+            })();
+
+            setTimeout(main, 5 * 60 * 1000);
+        }
+    }, timeout);
 }
 
-main()
+main();
